fb124829d72e1395e12e47dd4dd6710687ea8a2a
Ajay Banstola <ajay.banstola@gmail.com>
2019-07-06 06:32:26 +0545

    made a form

---
+++ src/core/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Jobs(models.Model):
+    #Id = models.IntegerField()
+    Job = models.CharField(max_length=250)
+    Company = models.TextField(max_length=250, default="Not Available")
+    Address = models.TextField(max_length=250, default="Not available")
+    Deadline = models.DateField(default=timezone.now)
+    URL = models.URLField(
+        max_length=128,
+        db_index=True,
+        unique=False,
+        blank=True
+    )
+
+    class Meta:
+        ordering = ('-Deadline',)
+
+    def __str__(self):
+        return self.Job
