191493c7051bc36f7536e1edadfb517ab8769152
Ajay Banstola <ajay.banstola@gmail.com>
2019-07-14 19:13:49 +0545

    filtered the icontains and displayed in html

--- src/core/views.py
+++ src/core/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from .models import Jobs
 
 # Create your views here.
 
 
 def BootstrapFilterView(request):
-
-    return render(request, "bootstrap_form.html", {})
+	qs = Jobs.objects.all()
+	title_contains_query = request.GET.get('title_contains')
+	id_exact_query = request.GET.get('id_exact')
+	#id_exact_query = request.GET.get('id_exact')
+	if title_contains_query !='' and title_contains_query is not None:
+		qs = qs.filter(Job__icontains = title_contains_query)
+	
+	
+	context = {
+		'queryset':qs
+	}
+	
+	return render(request, "bootstrap_form.html", context)
+	
