8f84a26ad228c93965a0fa7c9bb43c2547429434
Ajay Banstola <ajay.banstola@gmail.com>
2019-07-14 18:36:49 +0545

    added 3 categories

--- src/core/models.py
+++ src/core/models.py
@@ -3,21 +3,45 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class Category(models.Model):
+	name = models.CharField(max_length = 30)
+
+	def __str__(self):
+		return self.name
+
+
+class Company(models.Model):
+	name = models.CharField(max_length=20)
+
+	def __str__(self):
+		return self.name
+
+
 class Jobs(models.Model):
     #Id = models.IntegerField()
-    Job = models.CharField(max_length=250)
-    Company = models.TextField(max_length=250, default="Not Available")
-    Address = models.TextField(max_length=250, default="Not available")
-    Deadline = models.DateField(default=timezone.now)
-    URL = models.URLField(
-        max_length=128,
-        db_index=True,
-        unique=False,
-        blank=True
-    )
+	Job = models.CharField(max_length=250)
+	category = models.ForeignKey(Category, on_delete = models.CASCADE,null =True, blank = True)
+	company = models.ManyToManyField(Company)
+	Address = models.TextField(max_length=250)
+	views = models.IntegerField(default = 0)
+	Deadline = models.DateField(default=timezone.now)
+	employmentType = models.CharField(max_length=50, null = True, blank = True)
+	URL = models.URLField(
+		max_length=128,
+		db_index=True,
+		unique=False,
+		blank=True
+	)
+	#validThrough = models.CharField(max_length=140)
+	#datePosted = models.DateTimeField(default = false,blank=True)
+	#keywords = models.ArrayField(max_length=100)
 
-    class Meta:
-        ordering = ('-Deadline',)
+	class Meta:
+		ordering = ('-Deadline',)
 
-    def __str__(self):
-        return self.Job
+	def __str__(self):
+		return self.Job
+		
+
+
+
