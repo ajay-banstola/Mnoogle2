ebb021e86606de839f16839e8a1cd56ac9492c7f
Ajay Banstola <ajay.banstola@gmail.com>
2019-07-15 04:45:55 +0545

    category based searching working till here

--- src/core/views.py
+++ src/core/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render
-from .models import Jobs
+from .models import Jobs, Category
 
 # Create your views here.
 
+def is_valid_queryparam(param):
+	return param !='' and param is not None
 
 def BootstrapFilterView(request):
 	qs = Jobs.objects.all()
+	categories = Category.objects.all()
 	title_contains_query = request.GET.get('title_contains')
 	id_exact_query = request.GET.get('id_exact')
 	#id_exact_query = request.GET.get('id_exact')
+	category = request.GET.get('category')
 	if title_contains_query !='' and title_contains_query is not None:
 		qs = qs.filter(Job__icontains = title_contains_query)
 	
-	
+	if is_valid_queryparam(category) and category !='Choose...':
+		qs = qs.filter(category__name =category)
 	context = {
-		'queryset':qs
+		'queryset':qs,
+		'categories':categories
 	}
 	
 	return render(request, "bootstrap_form.html", context)
